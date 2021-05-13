e5d14f4da260e55b4a9ae14ea83d809a55d6d71a
maatheuss0 <82424913+maatheuss0@users.noreply.github.com>
2021-05-13 20:05:50 -0300

    Add files via upload

---
+++ nome_C#/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace nome_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = null;
+            int quantidade = 10;
+            nomes = new string[quantidade];
+
+            string comparar;
+
+            for (var i = 0; i <= (10 - 1); i++)
+            {
+                Console.Write("Digite o nome " + (i + 1) + ": ");
+                nomes[i] = Console.ReadLine().ToLower();
+            }
+
+            Console.Write("Digite um nome para que eu procure: ");
+            comparar = Console.ReadLine().ToLower();
+
+            int c = 0;
+
+            bool nomeAchado = false;
+
+            foreach (var item in nomes)
+            {
+                if (nomes[c] == comparar)
+                {
+                    nomeAchado = true;
+                }
+                c++;
+            }
+
+            if (nomeAchado == true)
+            {
+                Console.WriteLine("\nACHEI!!!!");
+            }
+            else
+            {
+                Console.WriteLine("\nNome não encontrado");
+            }
+        }
+    }
+}
---
+++ tabuada_C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabuada_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada iniciada!!!");
+
+            Console.WriteLine("Insira o número desejado: ");
+            int tab = Convert.ToInt16(Console.ReadLine());
+
+            for (int c = 1; c <= 10; c++)
+            {
+                Console.WriteLine(tab + " x " + c + " = " + tab * c);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
